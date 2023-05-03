865553d5cbf8c2c2be11c38c30739785730b78a1
lashasuxa <lashasukhashvili1995@gmail.com>
2023-05-03 01:19:06 -0400

    markup finished

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,35 @@ function App() {
             <div key={job.id} className="job">
                 <div className="job-left">
                   <img src={job.logo} alt="" />
-                  <div></div>
+                  <div>
+                    <div className="info-header">
+                      <div className="company-title">{job.company}</div>
+                     {job.new ? <div className="new-job">new</div> : null}   
+                     {/* conditional rendering */}
+                      {job.featured ? <div className="featured-job">featured</div>: null}
+                    </div>
+                    <h2 className='job-position'>{job.position}</h2>
+                    <div className="info-footer">
+                      <h3 className='extra'>{job.postedAt}</h3>
+                      <span className='circle'></span>
+                      <h3 className='extra'>{job.contract}</h3>
+                      <span className='circle'></span>
+                      <h3 className='extra'>{job.location}</h3>
+                      
+                    </div>
+                  </div>
                 </div>
-                <div className="job-right"></div>
+                <div className="job-right">
+                  <button className='prop-btn'>{job.role}</button>
+                  <button className='prop-btn'>{job.level}</button>
+                  {job.tools.map((tool) => {
+                    return (<button key={tool} className="prop-btn">{tool}</button>);
+                  })}
+                   {job.languages.map((language) => {
+                    return (<button key={language} className="prop-btn">{language}</button>);
+                  })}
+
+                </div>
             </div>
           );
         })}
