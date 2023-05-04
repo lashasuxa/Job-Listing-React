476993836b1bb8b16792b6f77621e8e84a72134c
lashasuxa <lashasukhashvili1995@gmail.com>
2023-05-04 11:49:05 -0400

    component created separately

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,51 @@
-import { useState } from 'react';
-import './App.css';
-import data from './data.json';
-import Remove from "./assets/icon-remove.svg"
+import { useState } from "react";
+import "./app.css";
+import data from "./data.json";
+import Filters from "./components/Filters";
 
 function App() {
   const [jobs, setJobs] = useState(data);
-  const [filterArray,setFilterArray]=useState([]);
-  console.log(filterArray)
+  const [filterArray, setFilterArray] = useState([]);
 
-  const choiceHandler = (obj)=>{
-    setFilterArray((state)=>{
-      return [...state,obj]
+  const filteredData =
+    filterArray.length === 0
+      ? jobs
+      : jobs.filter((job) => {
+          return filterArray.every((button) => {
+            return (
+              job[button.property] === button.value ||
+              job[button.property].includes(button.value)
+            );
+          });
+        });
+
+  const choiceHandler = (obj) => {
+    setFilterArray((state) => {
+      return [...state, obj];
     });
-  }
-  const buttonRemoveHandler =(index)=>{
-    const copy=[...filterArray];
-    copy.splice(index,1)
-    setFilterArray(copy)
-  }
-  const clearHandler=()=>{
-    setFilterArray([])
-  }
-
+  };
 
   return (
     <div className="main">
-      {filterArray.length>0 ? (<div className="filters">
-        <div className="choice-btns">
-          {filterArray.map((button, index)=>{
-            console.log(button)
-            return (
-              <button className='filter-btn' key={button.value}>
-                <h3>{button.value}</h3>
-                <div className='remove-icon-box' onClick={()=>buttonRemoveHandler(index)}>
-                  <img src={Remove} alt="" />
-                </div>
-              </button>
-            );
-          })}
-        </div>
-        <button className="clear-btn" onClick={clearHandler}>clear</button>
-      </div>) : null}
-      <section className="jobs"style={{marginTop:filterArray.length>0?"40px":"232px"}}>
-        {jobs.map((job) => {
+      {filterArray.length > 0 ? (
+        <Filters jobs={filterArray} updateFilterArray={setFilterArray} />
+      ) : null}
+      <section
+        className="jobs"
+        style={{ marginTop: filterArray.length > 0 ? "40px" : "232px" }}
+      >
+        {filteredData.map((job) => {
           return (
-            <div key={job.id} className="job">
+            <div className="job" key={job.id}>
               <div className="job-left">
-                <img src={job.logo} alt={`${job.company} logo`} />
+                <img src={job.logo} alt="" />
                 <div>
                   <div className="info-header">
                     <div className="company-title">{job.company}</div>
                     {job.new ? <div className="new-job">new</div> : null}
-                    {job.featured ? <div className="featured-job">featured</div> : null}
+                    {job.featured ? (
+                      <div className="featured-job">FEATURED</div>
+                    ) : null}
                   </div>
                   <h2 className="job-position">{job.position}</h2>
                   <div className="info-footer">
@@ -64,25 +58,59 @@ function App() {
                 </div>
               </div>
               <div className="job-right">
-                <button className="prop-btn" onClick={()=>choiceHandler({
-                  property:"role",
-                  value:job.role,
-                })}>{job.role}</button>
-                <button className="prop-btn" onClick={()=>choiceHandler({
-                  property:"level",
-                  value:job.level,
-                })}>{job.level}</button>
+                <button
+                  className="prop-btn"
+                  onClick={() =>
+                    choiceHandler({
+                      property: "role",
+                      value: job.role,
+                    })
+                  }
+                >
+                  {job.role}
+                </button>
+                <button
+                  className="prop-btn"
+                  onClick={() =>
+                    choiceHandler({
+                      property: "level",
+                      value: job.level,
+                    })
+                  }
+                >
+                  {job.level}
+                </button>
                 {job.tools.map((tool) => {
-                  return (<button key={tool} className="prop-btn" onClick={()=>choiceHandler({
-                    property:"tools",
-                    value:tool,
-                  })}>{tool}</button>);
+                  return (
+                    <button
+                      className="prop-btn"
+                      key={tool}
+                      onClick={() =>
+                        choiceHandler({
+                          property: "tools",
+                          value: tool,
+                        })
+                      }
+                    >
+                      {tool}
+                    </button>
+                  );
                 })}
                 {job.languages.map((language) => {
-                  return (<button key={language} className="prop-btn" onClick={()=>choiceHandler({
-                    property:"languages",
-                    value:language,
-                  })}>{language}</button>);
+                  return (
+                    <button
+                      className="prop-btn"
+                      key={language}
+                      onClick={() =>
+                        choiceHandler({
+                          property: "languages",
+                          value: language,
+                        })
+                      }
+                    >
+                      {language}
+                    </button>
+                  );
                 })}
               </div>
             </div>
